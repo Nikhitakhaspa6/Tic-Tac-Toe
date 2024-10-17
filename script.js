fb66917f9a8350dabb2439b67a32f3001f9e3b8e
Nikhitakhaspa6 <nikhitakhaspa@gmail.com>
2024-10-17 19:41:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -41,8 +41,8 @@ function handleCellClick(event) {
 
   board[index] = currentPlayer;
   cell.innerHTML = currentPlayer === 'X' 
-    ? `<i class="bi bi-x-lg text-danger icon-x"></i>` 
-    : `<i class="bi bi-circle text-primary icon-o"></i>`;
+    ? `<i class="bi bi-x-lg text-danger"></i>` 
+    : `<i class="bi bi-circle text-primary"></i>`;
 
   if (checkWinner()) {
     showMessage(`${currentPlayer} wins! 🎉`);
